de222deabec4f0746efe5211965ad2a5dde21d0a
ejrns222 <62171230+ejrns222@users.noreply.github.com>
2020-03-25 23:14:04 +0900

    UI 만드는 중

---
+++ Assets/Script/cButtonManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cButtonManager : MonoBehaviour
+{
+    private Transform[] _childTransform;
+    void Awake()
+    {
+        _childTransform = gameObject.GetComponentsInChildren<Transform>();
+    }
+
+    
+}
---
+++ Assets/Script/cContentsManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class cContentsManager : MonoBehaviour
+{
+    private Transform[] _childTransform;
+    void Awake()
+    {
+        _childTransform = new Transform[transform.childCount];
+        for (int i = 0; i < transform.childCount; i++)
+            _childTransform[i] = transform.GetChild(i);
+        
+    }
+
+    public void SetContentSwitch(int numContent)
+    {
+        if (numContent < 1)
+        {
+            Debug.Log("1이상만 입렵가능");
+            return;
+        }
+
+        foreach (var p in _childTransform)
+        {
+            p.gameObject.SetActive(false);
+        }
+        GameObject temp = _childTransform[numContent - 1].gameObject;
+        
+        if(!temp.activeSelf)
+            temp.SetActive(true);
+    }
+
+}
