8e456a2697c98c7e4ef5f87345ad8092905a2381
ejrns222 <62171230+ejrns222@users.noreply.github.com>
2020-03-29 01:53:41 +0900

    플레이어 제작중

---
+++ Assets/Script/Charactor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public abstract class Charactor
+{
+    protected uint Subscriber { get; set; } //나중에 set을 특정 효과에따라 증감하게 바꿔야 함
+    protected string Name;
+    protected string Desc;
+    
+    public abstract bool Init(string name, uint numSubs);
+   
+}
+
+public class Streamer : Charactor
+{
+    public override bool Init(string name, uint numSubs)
+    {
+        Name = name;
+        Subscriber = numSubs;
+        return true;
+    }
+}
+
+public class Neotuber : Charactor
+{
+    public override bool Init(string name, uint numSubs)
+    {
+        Name = name;
+        Subscriber = numSubs;
+        return true;
+    }
+}
+
+public class DDeokuni : Streamer
+{
+    public void Update(ref UInt64 mileage)
+    {
+        mileage *= 2;
+    }
+}
--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -5,12 +5,25 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-   public UInt64 Mileage { get; set; } //관리하는 캐릭터들의 강화, 캐스팅, 굿즈 구입에 사용되는 재화
+   private UInt64 _mileage;
+   public UInt64 Mileage 
+   {
+      get { return _mileage; }
+      set { _mileage = value; } 
+   } //관리하는 캐릭터들의 강화, 캐스팅, 굿즈 구입에 사용되는 재화
    public uint Jewel { get; set; } //유료재화
    public UInt64 Money { get; set; } //플레이어의 강화에 사용되는 재화
+
+   
    
    private static Player instance;
-
+   
+   ////////////////////////TEST///////////////////////////// 배열에 넣어서 스트리머와 유튜버를 관리하는 방식으로 만들자
+   private DDeokuni _deokuni;
+   /// //////////////////////////////////////////////////////
+  
+   
+   
    public static Player Instance
    {
       get
@@ -51,6 +64,9 @@ public class Player : MonoBehaviour
       Mileage = 0;
       Jewel = 0;
       Money = 0;
+      ////////////////////////TEST/////////////////////////////
+      _deokuni = new DDeokuni();
+      /// //////////////////////////////////////////////////
    }
 
    private void FixedUpdate()
@@ -58,6 +74,11 @@ public class Player : MonoBehaviour
       Mileage++;
       Jewel += 2;
       Money += 3;
+      
+      ////////////////////////TEST///////////////////////////// 프로퍼티로 사용할수 있게 바꿔보자. 혹은 1안. 마일리지 같은 재화를 클래스로 만들어서 재화가 증가하는 것을 계산해주는 함수를 만든다. 2안. 델리게이트를 만들고 각 캐릭터들이 계산함수를 들고있고 델리게이트에 전부 넣어버린다면?
+      _deokuni.Update(ref _mileage);
+      ///  /// //////////////////////////////////////////////////
+      
    }
 }
 
