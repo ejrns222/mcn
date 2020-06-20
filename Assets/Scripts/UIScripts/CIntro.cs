570046f58dcf117a616274782097490b72906e2f
ejrns222 <62171230+ejrns222@users.noreply.github.com>
2020-06-21 08:18:48 +0900

    간단하게 시작화면 넣음

---
+++ Assets/Scripts/UIScripts/CIntro.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class CIntro : MonoBehaviour
+{
+    private Text _startText;
+    private void Awake()
+    {
+        _startText = transform.Find("TouchToPlay").GetComponent<Text>();
+    }
+
+    private void Start()
+    {
+        StartCoroutine(nameof(FadeInOut)); // 어웨이크는 정확한 호출 시기를 알 수가 없기 떄문에 코루틴은 스타트에서 호출합시다.
+    }
+
+    IEnumerator FadeInOut()
+    {
+         
+        bool isFadeOut = true;
+        while (true)
+        {
+            var textColor = _startText.color;
+            if (isFadeOut)
+            {
+                textColor.a -= 0.1f;
+                if (textColor.a < 0.1f)
+                {
+                    isFadeOut = false;
+                }
+            }
+            else
+            {
+                textColor.a += 0.1f;
+                if (textColor.a > 0.9f)
+                {
+                    isFadeOut = true;
+                }
+            }
+            _startText.color = textColor;
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+    
+
+}
